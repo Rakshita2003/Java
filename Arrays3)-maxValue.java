7e04b6efebe7c234923457737430b011d0ee8f2f
Rakshita Gowda <101338848+Rakshita2003@users.noreply.github.com>
2023-02-22 21:05:45 +0530

    Create Arrays3)-maxValue.java

---
+++ Arrays3)-maxValue.java
@@ -0,0 +1,20 @@
+class Main{
+    static int max(int[] arr){
+    if(arr.length==0){
+        return -1;
+    }
+        int maxVal=arr[0];
+        for(int i=1;i<arr.length;i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+            return maxVal;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,23,9,18,99};
+
+        System.out.println(max(arr));
+    }
+}
