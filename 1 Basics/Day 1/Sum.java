c5329d9e62f7e1080f4ac792ec66d1240176344e
Rakshita <rakshitagowda22@gmail.com>
2023-06-29 22:03:08 +0530

    Day 1

--- 1 Basics/Day 1/Sum.java
+++ 1 Basics/Day 1/Sum.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class Sum {
+public class sum {
     public static void main (String [] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number: ");
